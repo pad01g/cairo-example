3f891f43c017c62ecd29a163bb34790d88a472b2
pad01g <101613202+pad01g@users.noreply.github.com>
2023-05-16 10:09:41 +0000

    parsing nonce is failing

--- rfc6979/rfc6979.py
+++ rfc6979/rfc6979.py
@@ -1,23 +1,73 @@
 from secp256k1 import PrivateKey, PublicKey
-import pprint, json
+from cffi import FFI  # noqa: E402
+from starkware.crypto.signature.signature import pedersen_hash
+import pprint, json, tempfile, sys
 
 key = '31a84594060e103f5a63eb742bd46cf5f5900d8406e2726dedfc61c7cf43ebad'
 msg = '9e5755ec2f328cc8635a55415d0e9a09c2b6f2c9b0343c945fbbfe08247a4cbe'
 # 30 44 02 20 132382ca59240c2e14ee7ff61d90fc63276325f4cbe8169fc53ade4a407c2fc8 02 20 4d86fbe3bde6975dd5a91fdc95ad6544dcdf0dab206f02224ce7e2b151bd82ab
-sig = '30440220132382ca59240c2e14ee7ff61d90fc63276325f4cbe8169fc53ade4a407c2fc802204d86fbe3bde6975dd5a91fdc95ad6544dcdf0dab206f02224ce7e2b151bd82ab'
+# sig = '30440220132382ca59240c2e14ee7ff61d90fc63276325f4cbe8169fc53ade4a407c2fc802204d86fbe3bde6975dd5a91fdc95ad6544dcdf0dab206f02224ce7e2b151bd82ab'
+
+# <!-- custom nonce function
+# https://github.com/rustyrussell/secp256k1-py/blob/5bad581d959d722bf6c2df5eaa996fd4c24096aa/tests/test_custom_nonce.py#L51
+ffi = FFI()
+ffi.cdef('static int nonce_function_rand(unsigned char *nonce32,'
+         'const unsigned char *msg32,const unsigned char *key32,'
+         'const unsigned char *algo16,void *data,unsigned int attempt);')
+# simple passthrough function
+ffi.set_source("_noncefunc",
+               """
+static int nonce_function_rand(unsigned char *nonce32,
+const unsigned char *msg32,
+const unsigned char *key32,
+const unsigned char *algo16,
+void *data,
+unsigned int attempt)
+{
+memcpy(nonce32,data,32);
+return 1;
+}
+               """)
+
+with tempfile.TemporaryDirectory() as build_temp:
+    ffi.compile(tmpdir=build_temp)
+
+    # Make sure we can find our nonce.
+    sys.path.append(build_temp)
+
+    import _noncefunc  # noqa: E402
+    from _noncefunc import ffi  # noqa: E402
+
+nf = ffi.addressof(_noncefunc.lib, "nonce_function_rand")
+# ndata is pedersen_hash(msg, private-key)
+nonce = pedersen_hash(
+    pedersen_hash(
+        int("0x"+msg[0:32], 16),
+        int("0x"+msg[32:64], 16)
+    ),
+    pedersen_hash(
+        int("0x"+key[0:32], 16),
+        int("0x"+key[32:64], 16)
+    )
+)
+nonce_bytes = nonce.to_bytes(32, 'little')
+print("nonce", nonce_bytes.hex())
+ndata = ffi.new("char [32]", nonce_bytes )
+# custom nonce function -->
 
 msg_ser = bytes(bytearray.fromhex(msg))
 privkey = PrivateKey(bytes(bytearray.fromhex(key)), raw=True)
 privkey_ser = privkey.private_key
 pubkey_ser = privkey.pubkey.serialize(compressed=False)
-sig_check = privkey.ecdsa_sign(bytes(bytearray.fromhex(msg)), raw=True)
+sig_check = privkey.ecdsa_sign(bytes(bytearray.fromhex(msg)), raw=True, custom_nonce=(nf, ndata))
 sig_ser = privkey.ecdsa_serialize(sig_check)
 
-assert sig_ser == bytes(bytearray.fromhex(sig))
+# assert sig_ser == bytes(bytearray.fromhex(sig))
+assert True == privkey.pubkey.ecdsa_verify(msg_ser, sig_check, raw=True)
 
 # pubkey has 65 byte length in DER encoding
 # signature has 70 bytes length in DER encoding https://crypto.stackexchange.com/questions/57731/ecdsa-signature-rs-to-asn1-der-encoding-question
-print(len(privkey_ser), len(pubkey_ser), len(sig_ser), len(msg_ser))
+print(len(privkey_ser), len(pubkey_ser), len(sig_ser), sig_ser.hex(), len(msg_ser))
 
 pprint.pprint({
     "msg_1": "0x" + msg_ser[0:16].hex(),
@@ -28,10 +78,15 @@ pprint.pprint({
     "pubkey_x_2": "0x" + pubkey_ser[16+1:32+1].hex(),
     "pubkey_y_1": "0x" + pubkey_ser[32+1:48+1].hex(),
     "pubkey_y_2": "0x" + pubkey_ser[48+1:64+1].hex(),
-    "sig_r_1": "0x" + sig_ser[0+4:16+4].hex(),
-    "sig_r_2": "0x" + sig_ser[16+4:32+4].hex(),
-    "sig_s_1": "0x" + sig_ser[32+6:48+6].hex(),
-    "sig_s_2": "0x" + sig_ser[48+6:64+6].hex(),
+    "sig_r_1": "0x" + sig_ser[0+5:16+5].hex(),
+    "sig_r_2": "0x" + sig_ser[16+5:32+5].hex(),
+    "sig_s_1": "0x" + sig_ser[32+7:48+7].hex(),
+    "sig_s_2": "0x" + sig_ser[48+7:64+7].hex(),
+    # "sig_r_1": "0x" + sig_ser[0+4:16+4].hex(),
+    # "sig_r_2": "0x" + sig_ser[16+4:32+4].hex(),
+    # "sig_s_1": "0x" + sig_ser[32+6:48+6].hex(),
+    # "sig_s_2": "0x" + sig_ser[48+6:64+6].hex(),
+
 })
 
 # {'msg_1': '0x9e5755ec2f328cc8635a55415d0e9a09',
--- rfc6979/src/rfc6979/rfc6979.cairo
+++ rfc6979/src/rfc6979/rfc6979.cairo
@@ -1,4 +1,4 @@
-%builtins range_check bitwise
+%builtins pedersen range_check bitwise
 
 from starkware.cairo.common.alloc import alloc
 from crypto.secp256k1_ecdsa import verify_ecdsa_secp256k1
@@ -10,12 +10,28 @@ from starkware.cairo.common.cairo_secp.signature import (
     get_generator_point,
     // div_mod_n,
 )
-from starkware.cairo.common.cairo_builtins import BitwiseBuiltin
+from starkware.cairo.common.cairo_builtins import (
+    HashBuiltin,
+    BitwiseBuiltin,
+)
+from starkware.cairo.common.hash import hash2
+from starkware.cairo.common.math import split_felt
+
+
+// Convert a felt to a Uint256
+//
+func felt_to_uint256{range_check_ptr}(value) -> Uint256 {
+    let (high, low) = split_felt(value);
+    let value256 = Uint256(high, low);
+    // let value256 = Uint256(low, high);
+    return value256;
+}
 
 // (1) (R,S) verifies for Q and m
 // (2) R = hmac-sha256(d,m) * G
 // (3) Q = d * G
 func main{
+    pedersen_ptr: HashBuiltin*,
     range_check_ptr,
     bitwise_ptr: BitwiseBuiltin*,
 }() {
@@ -28,11 +44,14 @@ func main{
 
     let pt = EcPoint(x_as_bigint3, y_as_bigint3);
 
-    let (r) = uint256_to_bigint( Uint256(0x276325f4cbe8169fc53ade4a407c2fc8, 0x132382ca59240c2e14ee7ff61d90fc63) );
-    let (s) = uint256_to_bigint( Uint256(0xdcdf0dab206f02224ce7e2b151bd82ab, 0x4d86fbe3bde6975dd5a91fdc95ad6544) );
-    let (z) = uint256_to_bigint( Uint256(0xc2b6f2c9b0343c945fbbfe08247a4cbe, 0x9e5755ec2f328cc8635a55415d0e9a09) );
+    let (r) = uint256_to_bigint( Uint256(0x492e341294025332d41a6cdd9fa01b1c, 0xf649260ca357bc6b3eceac8533f148c2) );
+    let (s) = uint256_to_bigint( Uint256(0x34501a2fa2d2a499e43502e6493ac220, 0x60560681be0c75043cc27fb5ee753b46) );
+    let msg_low = 0xc2b6f2c9b0343c945fbbfe08247a4cbe;
+    let msg_high = 0x9e5755ec2f328cc8635a55415d0e9a09;
+    let (z) = uint256_to_bigint( Uint256(msg_low, msg_high) );
     verify_ecdsa_secp256k1(pt, z, r, s);
     
+    // prepare for (2) and (3)
     local d_low;
     local d_high;
     %{
@@ -40,12 +59,44 @@ func main{
         ids.d_high = int(program_input["d_high"], 16)
     %}
     let (d) = uint256_to_bigint( Uint256(d_low, d_high) );
+    let (gen_pt: EcPoint) = get_generator_point();
 
     // (2)
+    let (message_hash) = hash2{hash_ptr=pedersen_ptr}(
+        x=msg_high, y=msg_low
+    );
+    let (privatekey_hash) = hash2{hash_ptr=pedersen_ptr}(
+        x=d_high, y=d_low
+    );
+    let (nonce) = hash2{hash_ptr=pedersen_ptr}(
+        x=message_hash, y=privatekey_hash
+    );
 
+    // convert nonce to element as little endian
+    let n_uint256 = felt_to_uint256(nonce);
+    let (n) = uint256_to_bigint(n_uint256);
+
+    %{
+        print("nonce", ids.nonce)
+        print("n", ids.n)
+        print("n_uint256.low", ids.n_uint256.low)
+        print("n_uint256.high", ids.n_uint256.high)
+    %}
+
+    with_attr error_message("Invalid nonce.") {
+        let (gen_n: EcPoint) = ec_mul(gen_pt, n);
+        %{
+            print("r.d0", ids.r.d0)
+            print("r.d1", ids.r.d1)
+            print("r.d2", ids.r.d2)
+            print("gen_n.x.d0", ids.gen_n.x.d0)
+            print("gen_n.x.d1", ids.gen_n.x.d1)
+            print("gen_n.x.d2", ids.gen_n.x.d2)
+        %}
+        assert r = gen_n.x;
+    }
 
     // (3)
-    let (gen_pt: EcPoint) = get_generator_point();
     with_attr error_message("Invalid key pair.") {
         let (gen_d: EcPoint) = ec_mul(gen_pt, d);
         assert pt.x = gen_d.x;
